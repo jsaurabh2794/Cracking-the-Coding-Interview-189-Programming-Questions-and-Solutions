241cb166b9ee9cfa8c19a96acfb098abfbf453c7
jsaurabh2794 <jsaurabh2794@gmail.com>
2019-10-24 22:09:34 +0530

    Stack&Queue

---
+++ src/utility/MyQueue.java
@@ -0,0 +1,53 @@
+package utility;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+
+	private static class QueueNode<T> {
+		public T data;
+		public QueueNode<T> next;
+
+		public QueueNode(T data) {
+			this.data = data;
+		}
+
+	}
+
+	private QueueNode<T> first;
+	private QueueNode<T> last;
+
+	public void add(T data) {
+		QueueNode<T> newNode = new QueueNode<T>(data);
+		if (first == null) {
+			first = newNode;
+			last = newNode;
+		}
+		if (last != null) {
+			last.next = newNode;
+		}
+	}
+
+	public T remove() {
+		if (last == null) {
+			throw new NoSuchElementException();
+		}
+		T data = first.data;
+		first = first.next;
+		if (first == null) {
+			last = null;
+		}
+		return data;
+	}
+
+	public T peek() {
+		if (first == null) {
+			throw new NoSuchElementException();
+		}
+		return first.data;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+}
