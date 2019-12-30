87b3b9f8df93b456f3635a50f429858570545b3d
jsaurabh2794 <jsaurabh2794@gmail.com>
2019-12-31 00:10:58 +0530

    Recursion_and_Dynamic_problem
    Signed-off-by: jsaurabh2794 <jsaurabh2794@gmail.com>

---
+++ src/Recursion_and_Dynamic_Programming/Permutations_without_Dups.java
@@ -0,0 +1,36 @@
+package Recursion_and_Dynamic_Programming;
+
+import java.util.ArrayList;
+
+public class Permutations_without_Dups {
+
+	public static void main(String[] args) {
+		System.out.println(getPermutation("Sakshi"));
+	}
+
+	private static ArrayList<String> getPermutation(String word) {
+
+		ArrayList<String> permutations = new ArrayList<String>();
+		if (word.length() == 0) {
+			permutations.add("");
+			return permutations;
+		}
+		char firstChar = word.charAt(0);
+		String remainder = word.substring(1);
+		ArrayList<String> words = getPermutation(remainder);
+		for (String w : words) {
+			for (int i = 0; i <=w.length(); i++) {
+				String s = insertCharAt(w, firstChar, i);
+				permutations.add(s);
+			}
+		}
+		return permutations;
+	}
+
+	private static String insertCharAt(String word, char firstChar, int i) {
+
+		String first = word.substring(0, i);
+		String end = word.substring(i);
+		return first + firstChar + end;
+	}
+}
